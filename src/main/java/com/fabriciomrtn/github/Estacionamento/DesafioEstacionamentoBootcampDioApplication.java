0661d1e34feff483a9e8d495ab4e87e8ca4f6570
ff <94084187+FabricioMrtn@users.noreply.github.com>
2022-11-19 18:27:00 -0300

    Primeiros arquivos

---
+++ src/main/java/com/fabriciomrtn/github/Estacionamento/DesafioEstacionamentoBootcampDioApplication.java
@@ -0,0 +1,13 @@
+package com.fabriciomrtn.github.Estacionamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioEstacionamentoBootcampDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioEstacionamentoBootcampDioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabriciomrtn/github/Estacionamento/DesafioEstacionamentoBootcampDioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabriciomrtn.github.Estacionamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioEstacionamentoBootcampDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
