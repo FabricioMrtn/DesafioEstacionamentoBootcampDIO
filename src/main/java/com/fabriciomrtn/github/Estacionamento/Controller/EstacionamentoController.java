4e61d9f6ae489ec7bc9b06b454bd5bd0a7978b42
ff <94084187+FabricioMrtn@users.noreply.github.com>
2022-11-23 19:38:37 -0300

    adicionando arquivo html e alteracao nos controllers

--- src/main/java/com/fabriciomrtn/github/Estacionamento/Controller/EstacionamentoController.java
+++ src/main/java/com/fabriciomrtn/github/Estacionamento/Controller/EstacionamentoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -41,7 +42,7 @@ public class EstacionamentoController {
 		List<EstacionamentoDTO> resultado = estacionamentoMapper.toEstacionamentoDTOList(listaEstacionamento);
 		return ResponseEntity.ok(resultado);
 	}
-	
+		
 	@GetMapping("/{id}")
 	@ApiOperation("Busca o carro pelo Id")
 	public ResponseEntity<EstacionamentoDTO> findById(@PathVariable String id){
